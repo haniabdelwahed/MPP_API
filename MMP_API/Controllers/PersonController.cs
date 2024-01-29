a6d0726a3c266a655ae6977c36781eed9d0f993a
haniabdelwahed <haniabdelwahed@outlook.com>
2024-01-29 20:55:58 +0200

    commit changes

---
+++ MMP_API/Controllers/PersonController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MMP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/<PersonController>
+        [HttpGet]
+        public IEnumerable<string> GetPersons()
+        {
+            return new string[] { "Test Person 1", "Test Person 1" };
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
